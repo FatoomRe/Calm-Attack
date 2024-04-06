e845b72e4e9943525ba0f7ef8e7fc9cb4709f2e4
Fatimah <Nyzak1@outlook.sa>
2024-04-06 13:46:26 +0300

    add startscreen file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:calmattack/start_screen.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'Debug Banner',
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          colors: [Color.fromARGB(255, 38, 59, 173), Color(0xff00c4b2)],
-          stops: [0.3, 2],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )),
-      ),
-    ),
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 38, 59, 173), Color(0xff00c4b2)],
+              stops: [0.3, 2],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+            child: const StartScreen())),
   ));
 }
-
---
+++ lib/start_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return const Center(child: Text('Start Screen'));
+  }
+}
