07b6791199a09cf3f6a813fbe705409d77a91180
Fatimah <Nyzak1@outlook.sa>
2024-05-04 23:24:08 +0300

    add ThirdScreen

--- lib/Pages/SecondScreen.dart
+++ lib/Pages/SecondScreen.dart
@@ -1,15 +1,30 @@
+import 'package:calmattack/Pages/ThirdScreen.dart';
 import 'package:flutter/material.dart';
 
+
 class SecondScreen extends StatelessWidget {
-  const SecondScreen ({super.key});
+  const SecondScreen({super.key});
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Column(
-        children: [
-          Text('welcome to Second Screen'),
-        ]),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text(
+            'Next',
+            style: TextStyle(
+              color: Color.fromARGB(255, 5, 0, 0),
+              fontSize: 20,
+            ),
+            ),
+          onPressed: () {
+            Navigator.push(context,
+            MaterialPageRoute(builder:(context) => const ThirdScreen()));
+          },
+        )),
     );
   }
 }
---
+++ lib/Pages/ThirdScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  Widget build(context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+    );
+  }
+}
