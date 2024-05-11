89369dc763f97477fb48bbdd108391d1624a45f5
Fatimah <Nyzak1@outlook.sa>
2024-05-12 00:37:14 +0300

    add text on Top

--- lib/Pages/ThirdScreen.dart
+++ lib/Pages/ThirdScreen.dart
@@ -6,9 +6,42 @@ class ThirdScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final Shader linearGradient = const LinearGradient(
+      colors: <Color>[Color(0xff3E3BD4), Color(0xff1AAC9B)],
+      stops: [0.1, 0.40],
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    ).createShader(const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Third Screen'),
+      backgroundColor: Colors.white, // Set the background color here
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 75),
+            const Text(
+              'Focus on',
+              style: TextStyle(
+                color: Color(0xff0F073E),
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ShaderMask(
+                shaderCallback: (bounds) => linearGradient,
+              child: const Text(
+                'Waves',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
