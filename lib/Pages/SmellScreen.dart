2a624633edcc150e6b53d4c46697afc68556ec2b
Fatimah <Nyzak1@outlook.sa>
2024-06-19 21:49:12 +0300

    edit smell ui

--- lib/Pages/SmellScreen.dart
+++ lib/Pages/SmellScreen.dart
@@ -3,21 +3,19 @@ import 'package:flutter/material.dart';
 
 class SmellScreen extends StatelessWidget {
   const SmellScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(top: 40, left: -60, child: Image.asset('assets/Smell/F1.png', width: 250, height: 250)),
-          Positioned(top: 40, right: -53, child: Image.asset('assets/Smell/F2.png', width: 250, height: 250)),
-          Positioned(top: 250, left: -29, child: Image.asset('assets/Smell/F3.png', width: 180, height: 180)),
-          Positioned(top: 250, right: -50, child: Image.asset('assets/Smell/F4.png', width: 180, height: 180)),
+          Positioned(top: 50, left: -60, child: Image.asset('assets/Smell/F1.png', width: 250, height: 250)),
+          Positioned(top: 50, right: -53, child: Image.asset('assets/Smell/F2.png', width: 250, height: 250)),
+          Positioned(top: 293, left: -29, child: Image.asset('assets/Smell/F3.png', width: 150, height: 150)),
+          Positioned(top: 293, right: -44, child: Image.asset('assets/Smell/F4.png', width: 160, height: 160)),
           Positioned(bottom: 90, left: -40, child: Image.asset('assets/Smell/F5.png', width: 230, height: 230)),
           Positioned(bottom:35, right: -84, child: Image.asset('assets/Smell/F6.png', width: 300, height:300)),
-          Positioned(bottom: 0, left: -20, child: Image.asset('assets/Smell/F7.png', width: 120, height: 120)),
+          Positioned(bottom: -15, left: -25, child: Image.asset('assets/Smell/F7.png', width: 130, height: 130)),
           Positioned(bottom: -7, right: -24, child: Image.asset('assets/Smell/F8.png', width: 140, height: 140)),
-
           Column(
             children: [
               const Spacer(flex: 7),
@@ -34,7 +32,7 @@ class SmellScreen extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 10),
-                      Text('Or smell any aviailable scent',
+                      Text('Or smell any available scent',
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           letterSpacing: 1.0,
@@ -77,7 +75,6 @@ class SmellScreen extends StatelessWidget {
                   ),
                 ],
               ),
-
               // Spacer to add some padding at the bottom
               const Spacer(),
             ],
