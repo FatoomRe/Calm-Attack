1a60dd00532dce453f3cccdae0982d60d326ec07
Fatimah <Nyzak1@outlook.sa>
2024-06-19 17:03:10 +0300

    add smell assets

--- lib/Pages/SmellScreen.dart
+++ lib/Pages/SmellScreen.dart
@@ -6,41 +6,10 @@ class SmellScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Set the main background color here
-      appBar: AppBar(
-        title: const Text('Smell'),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: const Center(
-        child: Column(
-          children: [
-            SizedBox(height: 40),
-            Text(
-              'Focus on',
-              style: TextStyle(
-                color: Color(0xff0F073E),
-                fontSize: 35,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Smell',
-              style: TextStyle(
-                color: Color(0xff0F073E),
-                fontSize: 35,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Coming Soon',
-              style: TextStyle(
-                color: Color(0xff0F073E),
-                fontSize: 20,
-              ),
-            ),
-          ],
-        ),
+      body: Stack(
+        children: [
+          
+        ],
       ),
     );
   }
