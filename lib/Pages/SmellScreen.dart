401baa8c538070b09b9338bab1fdaacee9d0285a
Fatimah <Nyzak1@outlook.sa>
2024-06-21 00:04:11 +0300

    -

--- lib/Pages/SmellScreen.dart
+++ lib/Pages/SmellScreen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class SmellScreen extends StatelessWidget {
   const SmellScreen({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return Scaffold(
       body: Stack(
         children: [
