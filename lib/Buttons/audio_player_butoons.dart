30f1664fb5f894aeb7dc5fd4eec552f26a9b4e61
Fatimah <Nyzak1@outlook.sa>
2024-07-11 10:58:51 +0300

    modified the code to make it clean

--- lib/Buttons/audio_player_butoons.dart
+++ lib/Buttons/audio_player_butoons.dart
@@ -12,112 +12,16 @@ class AudioPlayerButtons extends StatefulWidget {
 
 class _AudioPlayerButtonsState extends State<AudioPlayerButtons> {
   final player = AudioPlayer();
-  List<String> sounds = [
-    'music-1.mp3', //Waves
-    'music-2.mp3', //Rain
-    'music-3.mp3', //Birds
-    'music-4.mp3', //Fire
-    'music-5.mp3', //Forest
-    'music-6.mp3', //Wind
+  final List<String> sounds = [
+    'music-1.mp3', // Waves
+    'music-2.mp3', // Rain
+    'music-3.mp3', // Birds
+    'music-4.mp3', // Fire
+    'music-5.mp3', // Forest
+    'music-6.mp3', // Wind
   ];
   int currentIndex = 0;
   bool isPlaying = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      //color: Colors.red,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              color: const Color(0xffEBF4FD),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.7),
-                  spreadRadius: 0.1,
-                  blurRadius: 1,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: IconButton(
-              icon: const Icon(
-                Icons.skip_previous,
-                size: 40,
-                color: Color(0xff0F073E),
-              ),
-              onPressed: () {
-                playPreviousSound();
-              },
-            ),
-          ),
-          const SizedBox(width: 35),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              color: const Color(0xff0F073E),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.7),
-                  spreadRadius: 0.1,
-                  blurRadius: 1,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: IconButton(
-              icon: Icon(
-                isPlaying ? Icons.pause : Icons.play_arrow,
-                size: 40,
-                color: const Color(0xffEBF4FD),
-              ),
-              onPressed: () {
-                if (isPlaying) {
-                  pauseSound();
-                } else {
-                  playSound();
-                }
-              },
-            ),
-          ),
-          const SizedBox(width: 35),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              color: const Color(0xffEBF4FD),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.7),
-                  spreadRadius: 0.1,
-                  blurRadius: 1,
-                  offset: const Offset(0, 3),
-                ),
-              ],
-            ),
-            child: IconButton(
-              icon: const Icon(
-                Icons.skip_next,
-                size: 40,
-                color: Color(0xff0F073E),
-              ),
-              onPressed: () {
-                playNextSound();
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 
   @override
   void initState() {
@@ -125,43 +29,89 @@ class _AudioPlayerButtonsState extends State<AudioPlayerButtons> {
     playSound();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildIconButton(Icons.skip_previous, _playPreviousSound, const Color(0xffEBF4FD), const Color(0xff0F073E)),
+          const SizedBox(width: 35),
+          _buildIconButton(isPlaying ? Icons.pause : Icons.play_arrow, _togglePlayPause, const Color(0xff0F073E), const Color(0xffEBF4FD)),
+          const SizedBox(width: 35),
+          _buildIconButton(Icons.skip_next, _playNextSound, const Color(0xffEBF4FD), const Color(0xff0F073E)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIconButton(IconData icon, Function onPressed, Color backgroundColor, Color iconColor) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.7),
+            spreadRadius: 0.1,
+            blurRadius: 1,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: IconButton(
+        icon: Icon(icon, size: 40, color: iconColor),
+        onPressed: () => onPressed(),
+      ),
+    );
+  }
+
   Future<void> playSound() async {
-  try {
-    final sound = sounds[currentIndex];
-    await player.play(AssetSource(sound));
-    setState(() {
-      isPlaying = true;
-    });
-  } catch (e) {
-    print("Error playing sound: $e");
+    try {
+      final sound = sounds[currentIndex];
+      await player.play(AssetSource(sound));
+      setState(() {
+        isPlaying = true;
+      });
+    } catch (e) {
+      print("Error playing sound: $e");
+    }
   }
-}
 
-Future<void> pauseSound() async {
-  try {
+  Future<void> pauseSound() async {
+    try {
+      await player.pause();
+      setState(() {
+        isPlaying = false;
+      });
+    } catch (e) {
+      print("Error pausing sound: $e");
+    }
+  }
+
+  Future<void> _playNextSound() async {
     await player.pause();
-    setState(() {
-      isPlaying = false;
-    });
-  } catch (e) {
-    print("Error pausing sound: $e");
-  }
-}
-
-  Future<void> playNextSound() async {
-    player.pause();
     currentIndex = (currentIndex + 1) % sounds.length;
     await playSound();
     widget.onSoundIndexChanged(currentIndex);
   }
 
-  Future<void> playPreviousSound() async {
-    player.pause();
+  Future<void> _playPreviousSound() async {
+    await player.pause();
     currentIndex = (currentIndex - 1 + sounds.length) % sounds.length;
     await playSound();
     widget.onSoundIndexChanged(currentIndex);
   }
 
+  void _togglePlayPause() {
+    if (isPlaying) {
+      pauseSound();
+    } else {
+      playSound();
+    }
+  }
 
   @override
   void dispose() {
