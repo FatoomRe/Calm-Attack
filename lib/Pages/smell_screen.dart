26e6d9bfe1bf018abf020a8166a0eadad5515227
Fatimah <Nyzak1@outlook.sa>
2024-07-17 22:00:54 +0300

    improved the code to make the design responsive

--- lib/Pages/smell_screen.dart
+++ lib/Pages/smell_screen.dart
@@ -3,47 +3,117 @@ import 'package:flutter/material.dart';
 
 class SmellScreen extends StatelessWidget {
   const SmellScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(top: 50, left: -60, child: Image.asset('assets/Smell/F1.png', width: 250, height: 250)),
-          Positioned(top: 50, right: -53, child: Image.asset('assets/Smell/F2.png', width: 250, height: 250)),
-          Positioned(top: 293, left: -29, child: Image.asset('assets/Smell/F3.png', width: 150, height: 150)),
-          Positioned(top: 293, right: -44, child: Image.asset('assets/Smell/F4.png', width: 160, height: 160)),
-          Positioned(bottom: 90, left: -40, child: Image.asset('assets/Smell/F5.png', width: 230, height: 230)),
-          Positioned(bottom:35, right: -84, child: Image.asset('assets/Smell/F6.png', width: 300, height:300)),
-          Positioned(bottom: -15, left: -25, child: Image.asset('assets/Smell/F7.png', width: 130, height: 130)),
-          Positioned(bottom: -7, right: -24, child: Image.asset('assets/Smell/F8.png', width: 140, height: 140)),
-          Column( 
+          Positioned(
+            top: screenHeight * 0.08,
+            left: screenWidth * -0.01,
+            child: Image.asset(
+              'assets/Smell/F1.png',
+              width: screenWidth * 0.30,
+              height: screenHeight * 0.30,
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.08,
+            right: screenWidth * -0.00,
+            child: Image.asset(
+              'assets/Smell/F2.png',
+              width: screenWidth * 0.40,
+              height: screenHeight * 0.40,
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.44,
+            left: screenWidth * -0.08,
+            child: Image.asset(
+              'assets/Smell/F3.png',
+              width: screenWidth * 0.375,
+              height: screenHeight * 0.1875,
+            ),
+          ),
+          Positioned(
+            top: screenHeight * 0.44,
+            right: screenWidth * -0.12,
+            child: Image.asset(
+              'assets/Smell/F4.png',
+              width: screenWidth * 0.4,
+              height: screenHeight * 0.2,
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.09,
+            left: screenWidth * -0.00,
+            child: Image.asset(
+              'assets/Smell/F5.png',
+              width: screenWidth * 0.40,
+              height: screenHeight * 0.40,
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.01,
+            right: screenWidth * -0.06,
+            child: Image.asset(
+              'assets/Smell/F6.png',
+              width: screenWidth * 0.45,
+              height: screenHeight * 0.45,
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * -0.01875,
+            left: screenWidth * -0.09,
+            child: Image.asset(
+              'assets/Smell/F7.png',
+              width: screenWidth * 0.325,
+              height: screenHeight * 0.1625,
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * -0.0175,
+            right: screenWidth * -0.09,
+            child: Image.asset(
+              'assets/Smell/F8.png',
+              width: screenWidth * 0.35,
+              height: screenHeight * 0.175,
+            ),
+          ),
+          Column(
             children: [
               const Spacer(flex: 7),
-                Center(
-                  child: Column(
-                    children: [
-                      const Text('Imagine the smell of\nLavender',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          letterSpacing: 1.3,
-                          fontSize: 32,
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xff0F073E),
-                        ),
+              Center(
+                child: Column(
+                  children: [
+                    const Text(
+                      'Imagine the smell of\nLavender',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        letterSpacing: 1.3,
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xff0F073E),
                       ),
-                      const SizedBox(height: 10),
-                      Text('Or smell any available scent',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          letterSpacing: 1.0,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w800,
-                          color: Colors.grey[700],
-                        ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Or smell any available scent',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.grey[700],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
+              ),
               const Spacer(flex: 5),
               Column(
                 children: [
@@ -53,30 +123,36 @@ class SmellScreen extends StatelessWidget {
                     child: ElevatedButton(
                       onPressed: () {
                         Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FinishScreen()));
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const FinishScreen(),
+                          ),
+                        );
                       },
                       style: ElevatedButton.styleFrom(
-                  // ignore: deprecated_member_use
-                  backgroundColor: const Color(0xff0F073E),
-                  elevation: 9,
-                ),
-                child: const Text('      Next      ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
+                        // ignore: deprecated_member_use
+                        backgroundColor: const Color(0xff0F073E),
+                        elevation: 9,
+                      ),
+                      child: const Text(
+                        '      Next      ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
                   TextButton(
                     onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FinishScreen()));
-                },
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const FinishScreen(),
+                        ),
+                      );
+                    },
                     child: const Text('Finish Session'),
                   ),
                 ],
