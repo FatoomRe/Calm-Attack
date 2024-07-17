d8e5a2011b217b9ebe13580ba57def606a935bf3
Fatimah <Nyzak1@outlook.sa>
2024-07-17 21:37:02 +0300

    improved the code to make the design responsive

--- lib/Pages/taste_screen.dart
+++ lib/Pages/taste_screen.dart
@@ -8,87 +8,146 @@ class TasteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(top: -5, right: -29, child: Image.asset('assets/Taste/doughnut.png', width: 190, height: 190),),
-          Positioned(top: 85,left: -16,child: Image.asset('assets/Taste/pancake.png', width: 180, height: 180),),
-          Positioned(top: 200,right: -40,child: Image.asset('assets/Taste/cupcake.png', width: 200, height: 200),),
-          Positioned(bottom: 165,left: -33,child: Image.asset('assets/Taste/milkshake.png', width: 190, height: 190),),
-          Positioned(bottom: 70,right: -56,child: Image.asset('assets/Taste/IceCream.png', width: 225, height: 225),),
-          Positioned(bottom: 0,left: -24,child: Image.asset('assets/Taste/pineapple.png', width: 200, height: 200),),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final screenWidth = constraints.maxWidth;
+          final screenHeight = constraints.maxHeight;
 
-          Column(
+          return Stack(
             children: [
-              const Spacer(flex: 7), //to push Texts to the center
-              Center(
-                child: Column(
-                  children: [
-                    const Text(
-                      'Imagine the taste of\nLemon Ice Cream',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        letterSpacing: 1.3,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w900,
-                        color: Color(0xff0F073E),
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      'Or find something you can taste',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        letterSpacing: 1.0,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                  ],
+              Positioned(
+                top: screenHeight * -0.005,
+                right: screenWidth * -0.05,
+                child: Image.asset(
+                  'assets/Taste/doughnut.png',
+                  width: screenWidth * 0.40,
+                  height: screenWidth * 0.40,
                 ),
               ),
-              const Spacer(flex: 5),//to push buttons to the bottom
+              Positioned(
+                top: screenHeight * 0.1,
+                left: screenWidth * -0.05,
+                child: Image.asset(
+                  'assets/Taste/pancake.png',
+                  width: screenWidth * 0.40,
+                  height: screenWidth * 0.40,
+                ),
+              ),
+              Positioned(
+                top: screenHeight * 0.25,
+                right: screenWidth * -0.1,
+                child: Image.asset(
+                  'assets/Taste/cupcake.png',
+                  width: screenWidth * 0.50,
+                  height: screenWidth * 0.50,
+                ),
+              ),
+              Positioned(
+                bottom: screenHeight * 0.2,
+                left: screenWidth * -0.09,
+                child: Image.asset(
+                  'assets/Taste/milkshake.png',
+                  width: screenWidth * 0.50,
+                  height: screenWidth * 0.50,
+                ),
+              ),
+              Positioned(
+                bottom: screenHeight * 0.1,
+                right: screenWidth * -0.15,
+                child: Image.asset(
+                  'assets/Taste/IceCream.png',
+                  width: screenWidth * 0.60,
+                  height: screenWidth * 0.60,
+                ),
+              ),
+              Positioned(
+                bottom: screenHeight * 0.00,
+                left: screenWidth * -0.05,
+                child: Image.asset(
+                  'assets/Taste/pineapple.png',
+                  width: screenWidth * 0.40,
+                  height: screenWidth * 0.40,
+                ),
+              ),
+              
               Column(
                 children: [
-                  Container(
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SmellScreen()));
-                },
-                      style: ElevatedButton.styleFrom(
-                  // ignore: deprecated_member_use
-                  backgroundColor: const Color(0xff0F073E),
-                  elevation: 9,
-                ),
-                child: const Text('      Next      ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ),
+                  const Spacer(flex: 7), // to push Texts to the center
+                  Center(
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Imagine the taste of\nLemon Ice Cream',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            letterSpacing: 1.3,
+                            fontSize: 32,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0xff0F073E),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          'Or find something you can taste',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            letterSpacing: 1.0,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FinishScreen()));
-                },
-                    child: const Text('Finish Session'),
+                  const Spacer(flex: 5), // to push buttons to the bottom
+                  Column(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SmellScreen()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xff0F073E),
+                            elevation: 9,
+                          ),
+                          child: const Text(
+                            'Next',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const FinishScreen()),
+                          );
+                        },
+                        child: const Text('Finish Session'),
+                      ),
+                    ],
                   ),
+
+                  // Spacer to add some padding at the bottom
+                  const Spacer(),
                 ],
               ),
-
-              // Spacer to add some padding at the bottom
-              const Spacer(),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
